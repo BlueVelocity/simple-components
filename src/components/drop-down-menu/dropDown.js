e2e9f3a1bdd08e2dcedb576e5cca2fa5c65b2770
BlueVelocity <josephrodney@hotmail.com>
2024-01-28 03:19:50 -0500

    Close open drop down when clicking outside of dropdown

--- src/components/drop-down-menu/dropDown.js
+++ src/components/drop-down-menu/dropDown.js
@@ -1,13 +1,20 @@
 export default function () {
   const dropDowns = document.querySelectorAll(".drop-down-menu");
   dropDowns.forEach((currentElement) => {
-    currentElement.firstElementChild.addEventListener("click", () => {
+    currentElement.firstElementChild.addEventListener("click", (event) => {
+      event.stopPropagation();
       dropDowns.forEach((element) => {
         if (element !== currentElement) {
           element.classList.remove("clicked");
         }
       });
       currentElement.classList.toggle("clicked");
+    });
+
+    window.addEventListener("click", function () {
+      if (currentElement.classList.contains("clicked")) {
+        currentElement.classList.remove("clicked");
+      }
     });
   });
 }
