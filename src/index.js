b83a6594d073a81cc5840d523f497f1303bfc705
BlueVelocity <josephrodney@hotmail.com>
2024-02-05 19:08:15 -0500

    Fix code naming error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
 import loadDropDowns from "./components/drop-down-menu/dropDown";
 import loadMobileMenu from "./components/mobile-menu/mobileMenu";
-import loadimageSlider from "./components/sliding-frames/slidingFrames";
+import loadImageSlider from "./components/sliding-frames/slidingFrames";
 
 loadDropDowns();
 loadMobileMenu();
-loadimageSlider();
+loadImageSlider();
