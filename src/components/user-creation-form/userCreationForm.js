c8111c4d84a6c82d8504704f57804a6ccb44c54b
BlueVelocity <josephrodney@hotmail.com>
2024-02-06 00:43:40 -0500

    Set up boilerplate code for the user-creation javascript

--- src/components/user-creation-form/userCreationForm.js
+++ src/components/user-creation-form/userCreationForm.js
@@ -1,2 +1,31 @@
 export default function () {
+  const emailInput = document.getElementById("email");
+  const emailError = document.getElementById("email-error");
+
+  emailInput.addEventListener("input", () => {
+    if (emailInput.validity === false) {
+      showError();
+    } else {
+      emailError.textContent = "";
+    }
+  });
+
+  const countryInput = document.getElementById("country");
+  const countryError = document.getElementById("country-error");
+
+  const zipCodeInput = document.getElementById("zip-code");
+  const zipCodeError = document.getElementById("zip-code-error");
+
+  const passwordInput = document.getElementById("password");
+  const passwordError = document.getElementById("password-error");
+
+  password.addEventListener("input", () => console.log(password.value));
+
+  const passwordConfirmError = document.getElementById(
+    "password-confirm-error",
+  );
+
+  function showError() {
+    console.log("working");
+  }
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import "./style.css";
 import loadDropDowns from "./components/drop-down-menu/dropDown";
 import loadMobileMenu from "./components/mobile-menu/mobileMenu";
 import loadImageSlider from "./components/sliding-frames/slidingFrames";
+import loadUserCreationForm from "./components/user-creation-form/userCreationForm";
 
 loadDropDowns();
 loadMobileMenu();
 loadImageSlider();
+loadUserCreationForm();
